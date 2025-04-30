323a7b8626888de208a1f2ce9ed9fd1cca0bc3b6
tiyo5151 <s1F102400224@iniad.org>
2025-04-30 12:38:32 +0900

    level1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,17 @@
 import styles from './page.module.css';
 import { useState } from 'react';
 
+const directions = [
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, -1],
+  [0, 1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
+];
+
 export default function Home() {
   const [gameBoard, setGameBoard] = useState<number[][]>([
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -13,26 +24,45 @@ export default function Home() {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  const [turnColor, setTurnColor] = useState(1);
 
-  const getCellStyle = (value: number) => {
-    switch (value) {
-      case 1:
-        return { backgroundColor: 'black' };
-      case 2:
-        return { backgroundColor: 'white' };
-      default:
-        return { backgroundColor: 'green' };
-    }
+  const handleClick = (x: number, y: number) => {
+    // console.log(`Clicked cell at (${x}, ${y})`);
+    const newGameBoard = structuredClone(gameBoard);
+    if (newGameBoard[y][x] !== 0) return;
+    directions.forEach(([dy, dx]) => {
+      for (let i = 1; i < 8; i++) {
+        const ny = y + dy * i;
+        const nx = x + dx * i;
+        if (newGameBoard[ny] !== undefined && newGameBoard[ny][nx] === 0) break;
+        if (newGameBoard[ny] !== undefined && newGameBoard[ny][nx] === 2 / turnColor) {
+          if (newGameBoard[ny + dy] !== undefined && newGameBoard[ny + dy][nx + dx] === turnColor) {
+            for (let j = 0; j < i + 1; j++) {
+              const nny = y + dy * j;
+              const nnx = x + dx * j;
+              newGameBoard[nny][nnx] = turnColor;
+            }
+          }
+        }
+      }
+    });
+    setGameBoard(newGameBoard);
+    setTurnColor(2 / turnColor);
   };
-  // const handleClick = (y: number, x: number) => {
-  //   const newGameBoard = [...gameBoard];
-  // };
+
   return (
     <div className={styles.container}>
-      {gameBoard.map((row, rowIndex) => (
-        <div key={rowIndex} className={styles.row}>
-          {row.map((cell, cellIndex) => (
-            <div key={cellIndex} style={getCellStyle(cell)} className={styles.cell}></div>
+      {gameBoard.map((row, y) => (
+        <div key={y} className={styles.row}>
+          {row.map((cell, x) => (
+            <div key={x} className={styles.cell} onClick={() => handleClick(x, y)}>
+              {gameBoard[y][x] !== 0 && (
+                <div
+                  className={styles.stone}
+                  style={{ backgroundColor: cell === 1 ? '#000' : '#fff' }}
+                ></div>
+              )}
+            </div>
           ))}
         </div>
       ))}
