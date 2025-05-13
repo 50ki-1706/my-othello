20c53f01e2f3fee1114be62a7e02911e6f924a6e
tiyo5151 <s1F102400224@iniad.org>
2025-05-13 12:42:08 +0900

    level3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,26 +9,42 @@ import { Board } from './lib/constants';
 import { checkCount } from './utils/checkCount';
 
 export default function Home() {
-  const [board, setBoard] = useState<number[][]>(Board);
-  const [turnColor, setTurnColor] = useState(1);
+  const [board, setBoard] = useState<{ board: number[][]; color: number }>({
+    board: Board,
+    color: 1,
+  });
+  const [isGameOver, setIsGameOver] = useState(false);
 
   useEffect(() => {
-    const counts = checkCount(markValidMoves(board, turnColor));
-    if (counts.empty === 0 && counts.moves === 0) {
-      alert(`Game Over! Black: ${counts.black}, White: ${counts.white}`);
+    if (isGameOver) return;
+    const counts = checkCount(markValidMoves(board.board, board.color));
+    if (counts.moves === 0) {
+      setBoard((prev) => ({
+        ...prev,
+        color: 2 / prev.color,
+      }));
+      const newCounts = checkCount(markValidMoves(board.board, 2 / board.color));
+      if (newCounts.moves === 0) {
+        setIsGameOver(true);
+        alert(`Game Over! Black: ${counts.black}, White: ${counts.white}`);
+        return;
+      }
+      alert('Pass!');
     }
-  }, [board, turnColor]);
+  }, [board, isGameOver]);
 
   const handleClick = (x: number, y: number) => {
-    const newBoard: number[][] = structuredClone(board);
-    const [result, canPut] = putStone(newBoard, turnColor, x, y);
+    if (isGameOver) return;
+    const newBoard: number[][] = structuredClone(board.board);
+    if (newBoard[y][x] !== 0) return;
+    const [result, canPut] = putStone(newBoard, board.color, x, y);
     if (!canPut) return;
-    setTurnColor(2 / turnColor);
-    setBoard(result);
+    setBoard({ board: result, color: 2 / board.color });
+    console.table(newBoard);
   };
   return (
     <div className={styles.container}>
-      {markValidMoves(board, turnColor).map((row, y) => (
+      {markValidMoves(board.board, board.color).map((row, y) => (
         <div key={y} className={styles.row}>
           {row.map((cell, x) => (
             <div key={x} className={styles.cell} onClick={() => handleClick(x, y)}>
